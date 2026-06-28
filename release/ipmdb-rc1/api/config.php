e66a056977a031e5b72bf7e0a942b3982dc33c5f
ajfisherco <ajfisherco@gmail.com>
2026-06-28 04:25:08 -0700

    build: add RC1 config template

---
+++ release/ipmdb-rc1/api/config.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'dsn' => '',
+        'user' => '',
+        'pass' => '',
+    ],
+    'app' => [
+        'asset_prefix' => 'IPM',
+        'mail_from' => 'noreply@ajfisherco.com',
+        'mail_name' => 'IPMdb',
+        'admin_email' => 'ajfisherco@gmail.com',
+    ],
+];
